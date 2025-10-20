a2a05f7e98c1cc30d4b63e5629ce967b418bbc7a
Hatef Barin <amir.barin76@gmail.com>
2025-10-20 20:30:28 +0330

    feat(coaches): IsCoach permission class

---
+++ coaches/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+
+
+class IsCoach(BasePermission):
+    def has_permission(self, request, view):
+        return hasattr(request.user, "coach")
